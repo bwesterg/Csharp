23d920242eceebb2b95a13a653d1a75d8f9c91c8
bwesterg <benton.westergaard@gmail.com>
2021-07-21 13:47:11 -0600

    Working with string methods.

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,11 +6,46 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("    /|");
-            Console.WriteLine("   / | ");
-            Console.WriteLine("  /  | ");
-            Console.WriteLine(" /   | ");
-            Console.WriteLine("/____|");
+            string characterName = "Benton";
+            int characterAge = 37;
+
+            Console.WriteLine("My name is " + characterName);
+            Console.WriteLine("I am " + characterAge + " years old");
+            Console.WriteLine("It's almost my birthday");
+            Console.WriteLine("and on August 3");
+
+            characterAge = 38;
+
+            Console.WriteLine("I will be " + characterAge);
+            Console.WriteLine("but my name will still be " + characterName);
+
+            string phrase = "Bicycle Academy" +" is sweet";
+            char grade = 'C';
+            int age = -50;
+            double gpa = 12.20;
+            bool isMale = true; 
+
+            Console.WriteLine("I earned a " + grade);
+            Console.WriteLine(phrase.Length);
+
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Contains("sweet"));
+            Console.WriteLine(phrase.Contains("skiing"));
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase[2]);
+            Console.WriteLine(phrase[8]);
+            Console.WriteLine(phrase.IndexOf("Academy"));
+            Console.WriteLine(phrase.IndexOf(" "));
+            Console.WriteLine(phrase.IndexOf("z"));
+            Console.WriteLine(phrase.Substring(8));
+            Console.WriteLine(phrase.Substring(8, 3));
+
+
+
+
+
+
+
 
 
 
